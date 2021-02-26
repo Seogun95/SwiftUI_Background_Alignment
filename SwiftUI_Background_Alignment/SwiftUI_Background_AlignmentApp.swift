be195dcc7166d319c545700e8e69b3a03cff7f7a
SeonJungKim <ksj083895@gmail.com>
2021-02-26 20:04:48 +0500

    Initial Commit

---
+++ SwiftUI_Background_Alignment/SwiftUI_Background_AlignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Background_AlignmentApp.swift
+//  SwiftUI_Background_Alignment
+//
+//  Created by 김선중 on 2021/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Background_AlignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
